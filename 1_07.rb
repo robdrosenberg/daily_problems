898cf5a546ab7e0a8ee190bad2230ce9ffd63a25
robdrosenberg <robertdr85@gmail.com>
2018-01-07 17:27:05 -0800

    added comments and alternat solution using map !

--- 1_07.rb
+++ 1_07.rb
@@ -1,4 +1,19 @@
+# Read about the Ruby `map` method. Then refactor the code below using `map`.
+
+# numbers = [1, 2, 4, 2]
+# doubled_numbers = []
+# numbers.each do |number|
+#   doubled_numbers << number * 2
+# end
+# p doubled_numbers
+
+#refractored code
 numbers = [1, 2, 4, 2]
 double_numbers =[]
 numbers.map{|num| double_numbers << num*2}
 p double_numbers
+
+#solution using .map!
+numbers = [1,2,4,2]
+numbers.map!{|num| num*2}
+p numbers
