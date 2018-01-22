a19d4eb39a1e587981f4b40407373bd7865234cb
robdrosenberg <robertdr85@gmail.com>
2018-01-21 19:18:44 -0800

    Solved 1/21 daily problem

--- 1_21.rb
+++ 1_21.rb
@@ -6,3 +6,12 @@
 #   {id: 4, title: "Fracture", rating: 2.0}
 # ]
 # to create a new array of hashes that only contain movies with a rating less than 4.0.
+
+movies = [
+  {id: 1, title: "Die Hard", rating: 4.0},
+  {id: 2, title: "Bad Boys", rating: 5.0},
+  {id: 3, title: "The Chamber", rating: 3.0},
+  {id: 4, title: "Fracture", rating: 2.0}
+]
+
+p movies.select {|movie| movie[:rating] < 4.0}
