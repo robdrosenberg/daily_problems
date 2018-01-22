51a140d52657e5644d6d97c2d9998aee3658810a
robdrosenberg <robertdr85@gmail.com>
2018-01-21 18:36:03 -0800

    intial commit for 1/21

---
+++ 1_21.rb
@@ -0,0 +1,8 @@
+# Use the `select` method with the following array of hashes
+# movies = [
+#   {id: 1, title: "Die Hard", rating: 4.0},
+#   {id: 2, title: "Bad Boys", rating: 5.0},
+#   {id: 3, title: "The Chamber", rating: 3.0},
+#   {id: 4, title: "Fracture", rating: 2.0}
+# ]
+# to create a new array of hashes that only contain movies with a rating less than 4.0.
