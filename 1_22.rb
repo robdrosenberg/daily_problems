81c98040cc269641c32368464a0e3e6b5b183758
robdrosenberg <robertdr85@gmail.com>
2018-01-22 23:09:58 -0800

    Initial commit for 1/22 daily problem

---
+++ 1_22.rb
@@ -0,0 +1,11 @@
+# Use the `select` method combined with the `map` method to convert the array of hashes
+# movies = [
+#   {id: 1, title: "Die Hard", rating: 4.0},
+#   {id: 2, title: "Bad Boys", rating: 5.0},
+#   {id: 3, title: "The Chamber", rating: 3.0},
+#   {id: 4, title: "Fracture", rating: 2.0}
+# ]
+# 
+# into an array of hashes that only contain movie ids of the movies with the letter 'b' in the title (case insensitive). The result should look like:
+# [2, 3]
+
