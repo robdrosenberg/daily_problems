d6ca5593c9d9c6b6a7a32e3819c61414c9ae6be7
robdrosenberg <robertdr85@gmail.com>
2018-01-27 12:57:44 -0800

    Initial instructions for 1/25

---
+++ 1_25.rb
@@ -0,0 +1,7 @@
+# Read about the Ruby `reduce` method. Then refactor the code below using `reduce`.
+# numbers = [1, 2, 4, 2]
+# sum = 0
+# numbers.each do |number|
+#   sum += number
+# end
+# p sum
