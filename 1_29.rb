2ab40185bf4468a4ab8f2b85a78cb5f2294d7169
robdrosenberg <robertdr85@gmail.com>
2018-01-30 21:58:53 -0800

    Completed 1/29

--- 1_29.rb
+++ 1_29.rb
@@ -8,3 +8,13 @@
 # ]
 # and calculate the total amount of words for all items of type `:book`. The result should be
 # 3906
+
+items = [
+  {title: "NoDBA", words: 561, tags: ["nosql", "people", "orm"], type: :book},
+  {title: "Infodeck", words: 1145, tags: ["nosql", "writing"], type: :book},
+  {title: "OrmHate", words: 1718, tags: ["nosql", "orm"], type: :book},
+  {title: "ruby", words: 1313, tags: ["ruby"], type: :article},
+  {title: "DDD_Aggregate", words: 482, tags: ["nosql", "ddd"], type: :book}
+]
+
+p total_words = items.select{|item| item[:type] == :book}.map{|book| book[:words]}.reduce(:+)
