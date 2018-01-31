0a47dab5d8340838b12414d5a5964ae1f9ecc7e9
robdrosenberg <robertdr85@gmail.com>
2018-01-30 21:25:31 -0800

    Instructions for 1/29

---
+++ 1_29.rb
@@ -0,0 +1,10 @@
+# Use the methods `map`, `select`, and `reduce` with the following array of hashes:
+# items = [
+#   {title: "NoDBA", words: 561, tags: ["nosql", "people", "orm"], type: :book},
+#   {title: "Infodeck", words: 1145, tags: ["nosql", "writing"], type: :book},
+#   {title: "OrmHate", words: 1718, tags: ["nosql", "orm"], type: :book},
+#   {title: "ruby", words: 1313, tags: ["ruby"], type: :article},
+#   {title: "DDD_Aggregate", words: 482, tags: ["nosql", "ddd"], type: :book}
+# ]
+# and calculate the total amount of words for all items of type `:book`. The result should be
+# 3906
