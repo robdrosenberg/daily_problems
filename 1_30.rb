e93a046a32852ca1dab476f0b9d675cddf23169d
robdrosenberg <robertdr85@gmail.com>
2018-01-30 22:41:35 -0800

    Completed 1/30 had to learn .all?

--- 1_30.rb
+++ 1_30.rb
@@ -1,3 +1,11 @@
 # Write a method called `mutation?`, which will accept two strings as a parameter and return true if all the letters from the second string are contained within the first string, and false otherwise.
 # p mutation?("burly", "ruby")    #=> true
 # p mutation?("burly", "python")  #=> false
+
+def mutation?(str_one, str_two)
+  str_two.chars.all? {|char| str_one.include? char}
+end
+
+
+p mutation?("burly", "ruby")    #=> true
+p mutation?("burly", "python")  #=> false
