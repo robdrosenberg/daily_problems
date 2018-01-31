c038b5d1ece96fb368d695d9073d7ac2b4cb4689
robdrosenberg <robertdr85@gmail.com>
2018-01-30 21:24:19 -0800

    completed 1/28 daily problem

--- 1_28.rb
+++ 1_28.rb
@@ -3,4 +3,19 @@
 # Use the `reduce` method to perform the computation in your scrabble_score method. The code should work as follows:
 # p scrabble_score("scholar")  #=> 12
 
+def scrabble_score(str)
+  points = {
+    'a'=> 1, 'b'=> 3, 'c'=> 3, 'd'=> 2, 
+    'e'=> 1, 'f'=> 4, 'g'=> 2, 'h'=> 4, 
+    'i'=> 1, 'j'=> 8, 'k'=> 5, 'l'=> 1, 
+    'm'=> 3, 'n'=> 1, 'o'=> 1, 'p'=> 3, 
+    'q'=> 10, 'r'=> 1, 's'=> 1, 't'=> 1, 
+    'u'=> 1, 'v'=> 4, 'w'=> 4, 'x'=> 8, 
+    'y'=> 4, 'z'=> 10}
+  total_points = 0
+  str.each_char{|char| total_points += points["#{char}"]}
+  total_points
+end
 
+
+p scrabble_score("scholar")  #=> 12
