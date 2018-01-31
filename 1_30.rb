d6201439bd6dbce10a30fdd24dd5552c59134ae8
robdrosenberg <robertdr85@gmail.com>
2018-01-30 21:59:57 -0800

    Instructions for 1/30 daily problem

---
+++ 1_30.rb
@@ -0,0 +1,3 @@
+# Write a method called `mutation?`, which will accept two strings as a parameter and return true if all the letters from the second string are contained within the first string, and false otherwise.
+# p mutation?("burly", "ruby")    #=> true
+# p mutation?("burly", "python")  #=> false
