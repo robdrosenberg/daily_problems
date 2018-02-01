d9fab308f01c355935095a06f2f5d262d6bf159c
robdrosenberg <robertdr85@gmail.com>
2018-01-31 21:34:33 -0800

    Instructions for 1/31 daily problem

---
+++ 1_31.rb
@@ -0,0 +1,3 @@
+# Write a method called `sum_of_range`, which will accept an array containing two numbers, and return the sum of all of the whole numbers within the range of those numbers, inclusive.
+# p sum_of_range([1, 4])  #=> 10
+# p sum_of_range([4, 1])  #=> 10
