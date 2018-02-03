060599b63ca3430255ac4a13f190bd487acce77f
robdrosenberg <robertdr85@gmail.com>
2018-02-03 09:43:28 -0800

    Add instructions for 2/1

---
+++ 2_1.rb
@@ -0,0 +1,13 @@
+# Read about the Ruby `sort` method. Then sort the following array of hashes by age. (Note - do not use the `sort_by` method for this exercise)
+# people = [
+#   {"name" => "Saron", "age" => 34},
+#   {"name" => "Majora", "age" => 28},
+#   {"name" => "Danilo", "age" => 45}
+# ]
+
+
+people = [
+  {"name" => "Saron", "age" => 34},
+  {"name" => "Majora", "age" => 28},
+  {"name" => "Danilo", "age" => 45}
+]
