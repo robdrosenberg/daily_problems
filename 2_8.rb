b7fb3503101ecd468ad56fb6cf6d2f2fe76dc9aa
robdrosenberg <robertdr85@gmail.com>
2018-02-10 16:20:20 -0800

    Finished 2/8

---
+++ 2_8.rb
@@ -0,0 +1,11 @@
+# Read about the Ruby ternary operator. Then refactor the code below using a ternary operator.
+# x = 74
+# if x > 10
+#   result = "big number"
+# else
+#   result = "small number"
+# end
+# puts result
+
+x = 74
+p x > 10 ? "big number" : "small number"
